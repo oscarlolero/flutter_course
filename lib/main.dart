857cf4072a06fe9f3474e45b4d38b9617344fe7b
Oscar Montes <oscmcojc1@live.com.mx>
2020-03-24 16:54:56 -0600

    Cap 1 Ep 27

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,55 @@
+//Packages
 import 'package:flutter/material.dart';
+//Own files
+import './question.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return _MyAppState();
+  }
+}
+
+class _MyAppState extends State<MyApp> { //hacemos conexion con la clase de arriba, el _ hace que nos e pueda usar en otro archivo, se hace privada
+  var _questionIndex = 0; // o int
+
+  void _answerQuestion() {
+    setState(() {
+      _questionIndex++;
+    });
+    print(_questionIndex);
+  }
+
   @override
   Widget build(BuildContext context) {
+    var questions = ['Buenas?', 'Noches?'];
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text('Título de la app'),
         ),
-        body: Column(children: <Widget>[],),
+        body: Column(
+          children: <Widget>[
+            Question(
+              questions[_questionIndex]
+            ),
+            RaisedButton(
+              child: Text('Answer 1'),
+              onPressed: _answerQuestion,
+            ),
+            RaisedButton(
+              child: Text('Answer 2'),
+              onPressed: _answerQuestion,
+            ),
+            RaisedButton(
+              child: Text('Answer 3'),
+              onPressed: _answerQuestion,
+            ),
+          ],
+        ),
       ),
     );
   }
